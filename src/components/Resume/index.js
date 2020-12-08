4a4778f5dd4443aef16340a4f6296052ae1e43f3
Grant Emerson <grant.alex.emerson@gmail.com>
2020-12-08 16:38:28 -0600

    resume added

---
+++ src/components/Resume/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+
+var style = {
+    fontSize: "4rem",
+    padding: "3rem",
+    textAlign: "center",
+    fontWeight: 1
+}
+
+function Resume() {
+
+    return (
+        <Container id="resume">
+            <Row style={style} className="justify-content-md-center" >
+            download my full <a href="" class="link">  resume</a>
+            </Row>
+        </Container>
+    );
+
+}
+
+export default Resume;
